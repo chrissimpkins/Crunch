44187ce121f55047ade8267223c6f4c56895afe0
Chris Simpkins <git.simpkins@gmail.com>
2016-04-11 08:26:24 -0400

    updated user messages, added file testing/file size testing

--- src/crunch.sh
+++ src/crunch.sh
@@ -9,12 +9,13 @@ chug_title=" ______                         __
 # Message on application open (no arguments passed to script on initial open)
 if [[ $# -eq 0 ]]; then
 	printf '%s\n' "$chug_title"
-	echo "=================================================\n"
+	echo "Insane(ly slow but wicked good) PNG image optimization\n"
+	echo "======================================================\n"
 	echo "Copyright 2016 Christopher Simpkins"
 	echo "MIT License"
 	echo "Source: //github.com/chrissimpkins/Crunch\n"
 	echo "Issues: //github.com/chrissimpkins/Crunch/issues\n"
-	echo "=================================================\n"
+	echo "======================================================\n"
 	echo " \n"
 	echo "Drag and drop your PNG images on this window to begin."
 fi
@@ -42,7 +43,10 @@ do
     		echo " \n"
     		printf "Current size (kB): \t%.3f\n" $(bc -l <<< "$post_pngquant_size/1000")
     		echo " \n"
-       		echo "==> Running stage 2 optimizations (this could take awhile)...\n"
+       		echo "==> Running stage 2 optimizations...\n"
+       		if [[ $(bc -l <<< "$pre_size") -gt 35000 ]]; then
+    			echo "==> Hold tight.  This could take awhile.\n==> Click Cancel at any time to stop processing."
+    		fi
     		#./zopflipng --splitting=3 --filters=0meb -y --lossy_8bit --lossy_transparent "$imagepath" "${imagepath%%.*}-crunch.png" >/dev/null
     		./zopflipng -y --lossy_8bit --lossy_transparent "$imagepath" "${imagepath%%.*}-crunch.png" >/dev/null
     	else
@@ -50,7 +54,22 @@ do
     		echo " \n"
     		printf "Current size (kB): \t%.3f\n" $(bc -l <<< "$post_pngquant_size/1000")
     		echo " \n"
-    		echo "==> Running stage 2 optimizations (this could take awhile)...\n"
+    		echo "==> Running stage 2 optimizations...\n"
+    		if [[ $(bc -l <<< "$pre_size") -gt 35000 ]]; then
+    			echo "==> Hold tight.  This could take awhile.\n==> Click Cancel at any time to save at the above size."
+    		fi
+
+    		if [[ -f "$working_imagepath" ]]; then
+    			echo " " >/dev/null
+    		else
+    			sleep 2  # pause for two seconds for file write
+    			if [[ -f "$working_imagepath" ]]; then
+    				echo " " >/dev/null
+    			else
+    				echo "[ERROR] Unable to access the file for optimizations.  Please report this issue."
+    			fi
+    		fi
+
     		#./zopflipng --splitting=3 --filters=0meb -y --lossy_8bit --lossy_transparent "$working_imagepath" "$working_imagepath" >/dev/null
     		./zopflipng -y "$working_imagepath" "$working_imagepath" >/dev/null
     	fi
@@ -58,14 +77,14 @@ do
     	# get the post compression size of the image file
     	post_zopfli_size=$(stat -f %z "$working_imagepath")
     	echo " \n"
-    	printf "Final size (kB): \t%.3f\n" $(bc -l <<< "$post_zopfli_size/1000")
-    	echo " \n"
     	echo "---\n"
     	echo "Optimized filepath: $working_imagepath"
+    	printf "Final size  (kB): \t%.3f\n" $(bc -l <<< "$post_zopfli_size/1000")
     	printf "Percent original: \t%.2f%%\n" $(bc -l <<< "($post_zopfli_size/$pre_size) * 100")
     	printf "Space saved (kB): \t%.3f\n" $(bc -l <<< "($pre_size - $post_zopfli_size) / 1000")
     	echo "---\n"
+    	echo " \n"
     else
-    	echo "ALERT:$imagepath does not appear to be a png file"
+    	echo "ALERT:Unable to Process File|$imagepath does not appear to be a png file"
     fi
 done

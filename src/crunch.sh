d298bc7439c331e5b41b6c4fa20e94282989c529
Chris Simpkins <git.simpkins@gmail.com>
2018-04-04 23:58:21 -0400

    [crunch.sh] refactor for numerous POSIX compliance issues

--- src/crunch.sh
+++ src/crunch.sh
@@ -5,7 +5,7 @@ KERNEL=$(uname -s)
 
 filesize() {
     SIZE="";
-	if [ "Linux" = "$KERNEL" ]; then
+	if [ "$KERNEL" = "Linux" ]; then
 		SIZE=$(stat --printf="%s" "$1")
 	else
 		SIZE=$(stat -f %z "$1")
@@ -14,67 +14,57 @@ filesize() {
 	echo "$SIZE"
 }
 
-if [ "Linux" = "$KERNEL" ]; then
+if [ "$KERNEL" = "Linux" ]; then
 	alias echo='echo -e '
 fi
 
+# TODO: refactor this to test against the expected local build paths for these executables
 if ! command -v pngquant > /dev/null; then
-	echo "pngquant does not exist in \$PATH"
+	printf "pngquant was not found on your system! Optimization canceled.\\n"
 	exit 1;
 fi
 
 if ! command -v zopflipng > /dev/null; then
-	echo "zopflipng does not exist in \$PATH"
+	printf "zopflipng was not found on your system! Optimization canceled.\\n"
 	exit 1;
 fi
 
 for imagepath in "$@"
 do
     file_extension="${imagepath##*.}"
-    if [[ "$file_extension" = "png" ]]; then
-    	echo "• • • Crunching $imagepath...\n"
-    	echo " \n"
+    if [ "$file_extension" = "png" ]; then
+    	printf "• • • Crunching %s...\\n\\n" "$imagepath"
     	# get the pre compression size of the image file
     	pre_size=$(filesize "$imagepath")
-    	printf "Original size (kB): \t%.3f\n" $(bc -l <<< "$pre_size/1000")
+    	printf "Original size (kB): \\t%.3f\\n\\n" $(echo "$pre_size/1000" | bc -l)
 
     	working_imagepath="${imagepath%.png}-crunch.png"
 
     	# pngquant run on the file
-    	echo " \n"
-    	echo "==> Running stage 1 optimizations...\n"
+    	printf "==> Running stage 1 optimizations...\\n\\n"
     	pngquant --quality=80-98 --skip-if-larger --force --ext -crunch.png "$imagepath" >/dev/null
 
-    	if [[ $? -ne 0 ]]; then
+    	if [ $? -ne 0 ]; then
     		post_pngquant_size$(filesize "$imagepath")
-    		echo " \n"
-    		printf "Current size (kB): \t%.3f\n" $(bc -l <<< "$post_pngquant_size/1000")
-    		echo " \n"
-       		echo "==> Running stage 2 optimizations...\n"
-       		if [[ $(bc -l <<< "$pre_size") -gt 35000 ]]; then
-    			echo "==> Hold tight.  This could take awhile.\n==> Click Cancel at any time to stop processing."
+    		printf "Current size (kB): \\t%.3f\\n\\n" $(echo "$post_pngquant_size/1000" | bc -l)
+       		printf "==> Running stage 2 optimizations...\\n"
+       		if [ $(echo "$pre_size" | bc -l) -gt 35000 ]; then
+    			printf "==> Hold tight.  This could take awhile.\\n==> Click Cancel at any time to stop processing."
     		fi
     		#zopflipng --splitting=3 --filters=0meb -y --lossy_8bit --lossy_transparent "$imagepath" "${imagepath%%.*}-crunch.png" >/dev/null
     		zopflipng -y --lossy_8bit --lossy_transparent "$imagepath" "${imagepath%%.*}-crunch.png" >/dev/null
     	else
     		post_pngquant_size=$(filesize "$working_imagepath")
-    		echo " \n"
-    		printf "Current size (kB): \t%.3f\n" $(bc -l <<< "$post_pngquant_size/1000")
-    		echo " \n"
-    		echo "==> Running stage 2 optimizations...\n"
-    		if [[ $(bc -l <<< "$pre_size") -gt 35000 ]]; then
-    			echo "==> Hold tight.  This could take awhile.\n==> Click Cancel at any time to save at the above size."
+    		printf "Current size (kB): \\t%.3f\\n\\n" $(echo "$post_pngquant_size/1000" | bc -l)
+    		printf "==> Running stage 2 optimizations...\\n"
+    		if [ $(echo "$pre_size" | bc -l) -gt 35000 ]; then
+    			printf "==> Hold tight.  This could take awhile.\\n==> Click Cancel at any time to save at the above size."
     		fi
 
-    		if [[ -f "$working_imagepath" ]]; then
+    		if [ -f "$working_imagepath" ]; then
     			echo " " >/dev/null
     		else
-    			sleep 2  # pause for two seconds
-    			if [[ -f "$working_imagepath" ]]; then
-    				echo " " >/dev/null
-    			else
-    				echo "[ERROR] Unable to access the file for optimizations.  Please report this issue."
-    			fi
+    			printf "[ERROR] Unable to access the file for optimizations.  Please report this issue.\\n"
     		fi
 
     		#./zopflipng --splitting=3 --filters=0meb -y --lossy_8bit --lossy_transparent "$working_imagepath" "$working_imagepath" >/dev/null
@@ -83,15 +73,13 @@ do
 
     	# get the post compression size of the image file
     	post_zopfli_size=$(filesize "$working_imagepath")
-    	echo " \n"
-    	echo "---\n"
-    	echo "Optimized filepath: $working_imagepath"
-    	printf "Final size  (kB): \t%.3f\n" $(bc -l <<< "$post_zopfli_size/1000")
-    	printf "Percent original: \t%.2f%%\n" $(bc -l <<< "($post_zopfli_size/$pre_size) * 100")
-    	printf "Space saved (kB): \t%.3f\n" $(bc -l <<< "($pre_size - $post_zopfli_size) / 1000")
-    	echo "---\n"
-    	echo " \n"
+    	printf "\\n%s\\n" "---"
+    	printf "Optimized filepath: %s\\n" "$working_imagepath"
+    	printf "Final size  (kB): \\t%.3f\\n" $(echo "$post_zopfli_size/1000" | bc -l)
+    	printf "Percent original: \\t%.2f%%\\n" $(echo "($post_zopfli_size/$pre_size) * 100" | bc -l)
+    	printf "Space saved (kB): \\t%.3f\\n" $(echo "($pre_size - $post_zopfli_size) / 1000" | bc -l)
+    	printf "\\n%s\\n" "---"
     else
-    	echo "ALERT:Unable to Process File|$imagepath does not appear to be a png file"
+    	printf "[ERROR] %s does not appear to be a png file! Crunch does not work with other file types.\\n" "$imagepath"
     fi
 done

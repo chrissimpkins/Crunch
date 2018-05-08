12da9f926d8000c2b45b17bedeeda5eff7699a34
Chris Simpkins <git.simpkins@gmail.com>
2018-05-08 16:41:35 -0400

    [crunch-html.sh --> crunch-gui.sh] added parallel processing support to macOS GUI script

---
+++ src/crunch-gui.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# //////////////////////////////////////////////////////////
+#
+# crunch-gui.sh
+#  A shell script that executes Crunch PNG optimization
+#       for the Crunch macOS GUI application
+#  Copyright 2018 Christopher Simpkins and contributors
+#  MIT License
+#
+#  Source: https://github.com/chrissimpkins/Crunch
+#
+#  Note: this file is not intended for direct execution
+#        on the command line
+#
+# ///////////////////////////////////////////////////////////
+
+# Message on application open (no arguments passed to script on initial open)
+if [ $# -eq 0 ]; then
+    cat start.html
+    exit 0
+fi
+
+cat execution.html
+
+./crunch.py --gui "$@" &>/dev/null
+
+cat clear.html
+cat complete.html
+exit 0

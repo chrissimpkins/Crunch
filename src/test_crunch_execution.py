a2484cf5db270e0a91c47676c0c7947f4dda5540
Chris Simpkins <git.simpkins@gmail.com>
2018-05-09 11:09:52 -0400

    revised tests of service and gui flags to be platform specific tests

--- src/test_crunch_execution.py
+++ src/test_crunch_execution.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import platform
 import pytest
 
 import src.crunch
@@ -108,6 +109,22 @@ def test_crunch_function_get_zopflipng_path_gui():
     sys.argv = preargs
 
 
+def test_crunch_function_get_pngquant_path_service():
+    preargs = sys.argv
+    sys.argv = ["crunch.py", "--service", "test.png", "test2.png"]
+    response = src.crunch.get_pngquant_path()
+    assert response == "/Applications/Crunch.app/Contents/Resources/pngquant"
+    sys.argv = preargs
+
+
+def test_crunch_function_get_zopflipng_path_service():
+    preargs = sys.argv
+    sys.argv = ["crunch.py", "--service", "test.png", "test2.png"]
+    response = src.crunch.get_zopflipng_path()
+    assert response == "/Applications/Crunch.app/Contents/Resources/zopflipng"
+    sys.argv = preargs
+
+
 def test_crunch_function_optimize_png_unoptimized_file():
     startpath = os.path.join("testfiles", "robot.png")
     testpath = os.path.join("testfiles", "robot-crunch.png")
@@ -190,86 +207,90 @@ def test_crunch_function_main_multi_file():
 
 
 def test_crunch_function_main_single_file_with_gui_flag():
-    with pytest.raises(SystemExit):
-        startpath = os.path.join("testfiles", "robot.png")
-        testpath = os.path.join("testfiles", "robot-crunch.png")
-        # cleanup any existing files from previous tests
+    if platform.system() == "Darwin":
+        with pytest.raises(SystemExit):
+            startpath = os.path.join("testfiles", "robot.png")
+            testpath = os.path.join("testfiles", "robot-crunch.png")
+            # cleanup any existing files from previous tests
+            if os.path.exists(testpath):
+                os.remove(testpath)
+            src.crunch.main(["--gui", startpath])
+
+        # check for optimized file following execution
+        assert os.path.exists(testpath) is True
+
+        # cleanup optimized file produced by this test
         if os.path.exists(testpath):
             os.remove(testpath)
-        src.crunch.main(["--gui", startpath])
-
-    # check for optimized file following execution
-    assert os.path.exists(testpath) is True
-
-    # cleanup optimized file produced by this test
-    if os.path.exists(testpath):
-        os.remove(testpath)
 
 
 def test_crunch_function_main_single_file_with_service_flag():
-    with pytest.raises(SystemExit):
-        startpath = os.path.join("testfiles", "robot.png")
-        testpath = os.path.join("testfiles", "robot-crunch.png")
-        # cleanup any existing files from previous tests
+    if platform.system() == "Darwin":
+        with pytest.raises(SystemExit):
+            startpath = os.path.join("testfiles", "robot.png")
+            testpath = os.path.join("testfiles", "robot-crunch.png")
+            # cleanup any existing files from previous tests
+            if os.path.exists(testpath):
+                os.remove(testpath)
+            src.crunch.main(["--service", startpath])
+
+        # check for optimized file following execution
+        assert os.path.exists(testpath) is True
+
+        # cleanup optimized file produced by this test
         if os.path.exists(testpath):
             os.remove(testpath)
-        src.crunch.main(["--service", startpath])
-
-    # check for optimized file following execution
-    assert os.path.exists(testpath) is True
-
-    # cleanup optimized file produced by this test
-    if os.path.exists(testpath):
-        os.remove(testpath)
 
 
 def test_crunch_function_main_multi_file_with_gui_flag():
-    with pytest.raises(SystemExit):
-        startpath1 = os.path.join("testfiles", "robot.png")
-        startpath2 = os.path.join("testfiles", "cat.png")
-        testpath1 = os.path.join("testfiles", "robot-crunch.png")
-        testpath2 = os.path.join("testfiles", "cat-crunch.png")
+    if platform.system() == "Darwin":
+        with pytest.raises(SystemExit):
+            startpath1 = os.path.join("testfiles", "robot.png")
+            startpath2 = os.path.join("testfiles", "cat.png")
+            testpath1 = os.path.join("testfiles", "robot-crunch.png")
+            testpath2 = os.path.join("testfiles", "cat-crunch.png")
 
-        # cleanup any existing files from previous tests
+            # cleanup any existing files from previous tests
+            if os.path.exists(testpath1):
+                os.remove(testpath1)
+            if os.path.exists(testpath2):
+                os.remove(testpath2)
+
+            src.crunch.main(["--gui", startpath1, startpath2])
+
+        # check for optimized file following execution
+        assert os.path.exists(testpath1) is True
+        assert os.path.exists(testpath2) is True
+
+        # cleanup optimized file produced by this test
         if os.path.exists(testpath1):
             os.remove(testpath1)
         if os.path.exists(testpath2):
             os.remove(testpath2)
-
-        src.crunch.main(["--gui", startpath1, startpath2])
-
-    # check for optimized file following execution
-    assert os.path.exists(testpath1) is True
-    assert os.path.exists(testpath2) is True
-
-    # cleanup optimized file produced by this test
-    if os.path.exists(testpath1):
-        os.remove(testpath1)
-    if os.path.exists(testpath2):
-        os.remove(testpath2)
 
 
 def test_crunch_function_main_multi_file_with_service_flag():
-    with pytest.raises(SystemExit):
-        startpath1 = os.path.join("testfiles", "robot.png")
-        startpath2 = os.path.join("testfiles", "cat.png")
-        testpath1 = os.path.join("testfiles", "robot-crunch.png")
-        testpath2 = os.path.join("testfiles", "cat-crunch.png")
+    if platform.system() == "Darwin":
+        with pytest.raises(SystemExit):
+            startpath1 = os.path.join("testfiles", "robot.png")
+            startpath2 = os.path.join("testfiles", "cat.png")
+            testpath1 = os.path.join("testfiles", "robot-crunch.png")
+            testpath2 = os.path.join("testfiles", "cat-crunch.png")
 
-        # cleanup any existing files from previous tests
+            # cleanup any existing files from previous tests
+            if os.path.exists(testpath1):
+                os.remove(testpath1)
+            if os.path.exists(testpath2):
+                os.remove(testpath2)
+
+            src.crunch.main(["--service", startpath1, startpath2])
+
+        # check for optimized file following execution
+        assert os.path.exists(testpath1) is True
+        assert os.path.exists(testpath2) is True
+
+        # cleanup optimized file produced by this test
         if os.path.exists(testpath1):
             os.remove(testpath1)
         if os.path.exists(testpath2):
             os.remove(testpath2)
-
-        src.crunch.main(["--service", startpath1, startpath2])
-
-    # check for optimized file following execution
-    assert os.path.exists(testpath1) is True
-    assert os.path.exists(testpath2) is True
-
-    # cleanup optimized file produced by this test
-    if os.path.exists(testpath1):
-        os.remove(testpath1)
-    if os.path.exists(testpath2):
-        os.remove(testpath2)

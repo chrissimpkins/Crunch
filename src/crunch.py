49c2bb1fb8153607437a83c96a59c5e78799e90d
Chris Simpkins <git.simpkins@gmail.com>
2018-05-18 22:38:34 -0400

    [crunch.py] removed .png extension testing, added png byte signature testing

--- src/crunch.py
+++ src/crunch.py
@@ -14,6 +14,7 @@
 import sys
 import os
 import shutil
+import struct
 import subprocess
 from subprocess import CalledProcessError
 
@@ -36,7 +37,7 @@ PNGQUANT_CLI_PATH = os.path.join(os.path.expanduser("~"), "pngquant", "pngquant"
 ZOPFLIPNG_CLI_PATH = os.path.join(os.path.expanduser("~"), "zopfli", "zopflipng")
 
 # Application Constants
-VERSION = "2.0.2"
+VERSION = "2.1.0"
 VERSION_STRING = "crunch v" + VERSION
 
 HELP_STRING = """
@@ -108,9 +109,8 @@ def main(argv):
     # COMMAND LINE ERROR HANDLING
     # //////////////////////////////////
 
-    # PNG file path error handling
-
     for png_path in png_path_list:
+        # Not a file test
         if not os.path.isfile(png_path):  # is not an existing file
             sys.stderr.write(
                 "[ERROR] '"
@@ -119,13 +119,9 @@ def main(argv):
                 + os.linesep
             )
             sys.exit(1)
-        elif not png_path[-4:] == ".png":  # does not end with .png extension
-            sys.stderr.write(
-                "[ERROR] '"
-                + png_path
-                + "' does not appear to be a PNG image file"
-                + os.linesep
-            )
+        # PNG validity test
+        if not is_valid_png(png_path):
+            sys.stderr.write("[ERROR] '" + png_path + "' is not a valid PNG file." + os.linesep)
             sys.exit(1)
 
     # Dependency error handling
@@ -172,7 +168,7 @@ def main(argv):
         except Exception as e:
             lock.acquire()
             sys.stderr.write("-----" + os.linesep)
-            sys.stderr.write("[ERROR] Error detected during execution of request:" + os.linesep)
+            sys.stderr.write("[ERROR] Error detected during execution of the request." + os.linesep)
             sys.stderr.write(str(e) + os.linesep)
             lock.release()
             sys.exit(1)
@@ -266,6 +262,16 @@ def get_zopflipng_path():
         return "/Applications/Crunch.app/Contents/Resources/zopflipng"
     else:
         return ZOPFLIPNG_CLI_PATH
+
+
+def is_valid_png(filepath):
+    # The PNG byte signature (https://www.w3.org/TR/PNG/#5PNG-file-signature)
+    expected_signature = struct.pack('8B', 137, 80, 78, 71, 13, 10, 26, 10)
+    # open the file and read first 8 bytes
+    with open(filepath, 'rb') as filer:
+        signature = filer.read(8)
+    # return boolean test result for first eight bytes == expected PNG byte signature
+    return signature == expected_signature
 
 
 def shellquote(filepath):
